3ca8e7c95ceaf41a4d62b18b11cf363e42f701e3
root <root@ip-172-31-20-95.ec2.internal>
2024-05-01 00:07:48 +0000

    first commit

---
+++ download_s3.py
@@ -0,0 +1,17 @@
+import boto3
+
+def download_file_from_s3(bucket_name, s3_object_key, local_file_path):
+    s3 = boto3.client('s3')
+    try:
+        s3.download_file(bucket_name, s3_object_key, local_file_path)
+        print(f"File downloaded successfully: {local_file_path}")
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+# Example usage
+bucket_name = 'myimages99'
+s3_object_key = 'Ranadheer_Are_amazon.pdf'
+local_file_path = '/home/ec2-user/downloaded_file.txt'  # Adjust the path as needed
+
+download_file_from_s3(bucket_name, s3_object_key, local_file_path)
+
